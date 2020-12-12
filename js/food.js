71c9d4d53a2c52527d87a0c68eef60344c5ea881
Kartikeyavasthi17 <71058480+Kartikeyavasthi17@users.noreply.github.com>
2020-12-12 07:52:21 -0800

    Add files via upload

---
+++ js/food.js
@@ -0,0 +1,57 @@
+class food{
+
+    constructor()
+    {
+        this.lastFed;
+        this.foodStock =20;
+
+        this.image =loadImage("Milk.png");
+    }
+
+    display()
+    {
+      var x =80,y=100;
+    
+      
+      if(this.foodStock!=0)
+      {
+        
+        for(var i=0; i<this.foodStock; i++)
+        {
+          if(i%10 == 0)
+          {
+            x=80;
+            y=y+50;
+          }
+          image(this.image,x,y,50,50);
+          x=x+30;
+        }
+      }
+      
+    }
+    
+
+    getfood()
+    {
+      foodStock =database.ref("Food");
+    foodStock.on("value",function(data){
+      this.foodStock =data.val();
+    })
+    }
+
+
+     updateFood()
+{
+  if(this.foodStock <= 0)
+  {
+    this.foodStock=0;
+  }else{
+    this.foodStock=this.foodStock-1;
+  }
+  
+  database.ref('/').update({
+    Food:this.foodStock
+  })
+
+}
+}
